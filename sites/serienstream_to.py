e9f780280e0f5e24664ca813eb255d5272eed0b3
DWH-WFC <45105289+DWH-WFC@users.noreply.github.com>
2022-12-06 19:52:03 +0100

    Domain Auswahl bei Serienstream (serien.cam) entfernt

--- sites/serienstream_to.py
+++ sites/serienstream_to.py
@@ -4,6 +4,7 @@
 #
 
 from operator import truediv
+import time
 from resources.lib.handler.ParameterHandler import ParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.tools import logger, cParser
@@ -15,12 +16,12 @@ from resources.lib.config import cConfig
 SITE_IDENTIFIER = 'serienstream_to'
 SITE_NAME = 'SerienStream'
 SITE_ICON = 'serienstream.png'
-SITE_SETTINGS = '<setting default="s.to" enable="!eq(-2,false)" id="serienstream_to-domain" label="30051" type="labelenum" values="s.to|serienstream.to|serien.cam|190.115.18.20" />'
+SITE_SETTINGS = '<setting default="s.to" enable="!eq(-2,false)" id="serienstream_to-domain" label="30051" type="labelenum" values="s.to|serienstream.to|190.115.18.20" />'
 domain = cConfig().getSetting('serienstream_to-domain')
 #SITE_SETTINGS = '<setting id="serienstream.user" type="text" label="30083" default="" /><setting id="serienstream.pass" type="text" option="hidden" label="30084" default="" />'
 SITE_GLOBAL_SEARCH = True
 
-#URL_MAIN = 'https://serien.cam/'
+#URL_MAIN = 'https://s.to/'
 if domain == "190.115.18.20":
     URL_MAIN = 'http://' + domain
     proxy = 'true'
@@ -33,7 +34,7 @@ URL_NEW_SERIES = URL_MAIN + '/neu'
 URL_NEW_EPISODES = URL_MAIN + '/neue-episoden'
 URL_POPULAR = URL_MAIN + '/beliebte-serien'
 URL_LOGIN = URL_MAIN + '/login'
-URL_SEARCH = 'https://s.to/ajax/search'
+URL_SEARCH = URL_MAIN + '/ajax/search'
 
 
 def load():
@@ -334,30 +335,36 @@ def showSearch():
 
 
 def _search(oGui, sSearchText):
+    SSsearch(oGui, sSearchText)
+
+
+def SSsearch(sGui=False, sSearchText=False):
     from json import loads
-    oGui = cGui()
+    oGui = sGui if sGui else cGui()
     params = ParameterHandler()
     params.getValue('sSearchText')
 
 
-    oRequest = cRequestHandler(URL_SEARCH, caching=True)
+    oRequest = cRequestHandler(URL_SEARCH, caching=False, ignoreErrors=(sGui is not False))
     oRequest.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
     oRequest.addHeaderEntry('Referer', 'https://s.to/search')
     oRequest.addHeaderEntry('Origin', 'https://s.to')
     oRequest.addParameters('keyword', sSearchText)
 
     sHtmlContent = oRequest.request()
+    time.sleep(3)
     if not sHtmlContent:
             return
 
 
     sst = sSearchText.lower()
 
-    j = loads(sHtmlContent)
-    total = len(j)
-    for a in j:
+    jload = loads(sHtmlContent)
+    total = len(jload)
+    for a in jload:
         if 'support' in a.get('link'):
             continue
+      
         sName = a.get('title').replace('/', '').replace('<em>', '')
         
         sLink = a.get('link')
@@ -375,3 +382,5 @@ def _search(oGui, sSearchText):
             params.setParam('sUrl', URL_MAIN + sLink)
             params.setParam('TVShowTitle', sName)
             oGui.addFolder(oGuiElement, params, True, total)
+        if not sGui:
+            oGui.setView('tvshows')
